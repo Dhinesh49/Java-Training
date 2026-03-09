bbcfa7790bfddc69908dde12bed8194d1c1dc8cd
Dhinesh P <dhineshdhinu8633@gmail.com>
2026-03-09 17:54:42 +0530

    Create Longest Harmonious Subsequence.java

---
+++ DAY4/Longest Harmonious Subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findLHS(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int longest = 0;
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int key : map.keySet()) {
+            if (map.containsKey(key + 1)) {
+                longest = Math.max(longest, map.get(key) + map.get(key + 1));
+            }
+        }
+
+        return longest;
+    }
+}
